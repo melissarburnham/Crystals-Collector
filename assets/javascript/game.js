9afb8a869af36e7e19331d84b3fcc73965cf702a
Melissa Burnham <melissaburnham@Melissas-MacBook-Air.local>
2018-02-07 18:26:24 -0500

    finished javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,104 @@
 var winCount = 0;
 var lossCount = 0;
-
+var targetNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+var userNumber = 0
+var images =["assets/images/crystalOne.jpg", "assets/images/crystalTwo.jpg", "assets/images/crystalThree.jpg", "assets/images/crystalFour.jpg"];
 //functions
 
-//adds wins
+$(document).ready(function(){
 
-//adds losses
+function setTargetNumber(){
+    $("#targetNumber").text(targetNumber);
+}
 
-//create on.click for each jewel
+function crystalValues(){
+for(i = 0; i < 4; i++){
+    var randomNumber = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+    var imageCrystal = $("<img>");
+    imageCrystal.addClass("crystal-image");
+    imageCrystal.attr("src", images[i]);
+    imageCrystal.attr("data-crystalvalue", randomNumber);
+    $("#crystals").append(imageCrystal);
 
-function crystalClick(){
-    $("#crystalOne").on("click", function() {
-        var randomOne = Math.floor(Math.random() * 10) + 1;
-        console.log(randomOne);
-        $("#crystalOne").append(randomOne);
-    });
+    };
+    console.log(userNumber);
+}
 
-    $("#crystalTwo").on("click", function() {
-        var randomTwo = Math.floor(Math.random() * 10) + 1;
-        console.log(randomTwo);
-        $("#crystalTwo").append(randomTwo);
-    });
+function clickCrystal(){
+$(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
 
-    $("#crystalThree").on("click", function() {
-        var randomThree = Math.floor(Math.random() * 10) + 1;
-        console.log(randomThree);
-        $("#crystalThree").append(randomThree);
-    });
+    userNumber += crystalValue;
+    $("#userScore").text(userNumber);
 
-    $("#crystalFour").on("click", function() {
-        var randomFour = Math.floor(Math.random() * 10) + 1;
-        console.log(randomFour);
-        $("#crystalFour").append(randomFour);
+    if (userNumber === targetNumber) {
+        alert("You win!");
+        winCount++;
+        newRound();
+      }
+    
+      else if (userNumber >= targetNumber) {
+        alert("You lose!!");
+        lossCount++;
+        newRound();
+        } 
+
+    $("#wins").text(winCount);
+    $("#losses").text(lossCount);
     });
 }
-crystalClick();
+
+function newRound(){
+    targetNumber =  Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    userNumber = 0;
+    setTargetNumber();
+    $(".crystal-image").remove();
+    crystalValues();
+    clickCrystal();
+}
+
+newRound();
+
+
+})
+
+
+
+
+
+
+
+
+
+
+// function trackWinOrLoss(){
+
+
+// trackWinOrLoss();
+
+
+
+// // dataattribute
+//     $("#crystalTwo").on("click", function() {
+//         var randomTwo = Math.floor(Math.random() * 10) + 1;
+//         console.log(randomTwo);
+//         $("#crystalTwo").append(randomTwo);
+//     });
+
+//     $("#crystalThree").on("click", function() {
+//         var randomThree = Math.floor(Math.random() * 10) + 1;
+//         console.log(randomThree);
+//         $("#crystalThree").append(randomThree);
+//     });
+
+//     $("#crystalFour").on("click", function() {
+//         var randomFour = Math.floor(Math.random() * 10) + 1;
+//         console.log(randomFour);
+//         $("#crystalFour").append(randomFour);
+//     });
+
+
 
 
 //beggining of each round
